4679cd288184eab7ef3e8a7fb159d597ddc487dc
Paul Jenkins <paul@thewoodknight.com>
2019-01-29 09:04:24 +1100

    Draft "Editor + Playback" UI concept

---
+++ TWKPrompter/Models/Item.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TWKPrompter.Models
+{
+    //https://stackoverflow.com/questions/6415037/populate-treeview-from-list-of-file-paths-in-wpf
+
+    public class Item
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+    }
+
+    public class FileItem : Item
+    {
+
+    }
+
+    public class DirectoryItem : Item
+    {
+        public List<Item> Items { get; set; }
+
+        public DirectoryItem()
+        {
+            Items = new List<Item>();
+        }
+    }
+
+    public class ItemProvider
+    {
+        public List<Item> GetItems(string path)
+        {
+            var items = new List<Item>();
+
+            var dirInfo = new DirectoryInfo(path);
+
+            foreach (var directory in dirInfo.GetDirectories())
+            {
+                var item = new DirectoryItem
+                {
+                    Name = directory.Name,
+                    Path = directory.FullName,
+                    Items = GetItems(directory.FullName)
+                };
+
+                items.Add(item);
+            }
+
+            foreach (var file in dirInfo.GetFiles())
+            {
+                var item = new FileItem
+                {
+                    Name = file.Name,
+                    Path = file.FullName
+                };
+
+                items.Add(item);
+            }
+
+            return items;
+        }
+    }
+}
---
+++ TWKPrompter/Views/MainView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Input;
+using TWKPrompter.Models;
+
+namespace TWKPrompter.Views
+{
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+
+            var itemProvider = new ItemProvider();
+
+            var items = itemProvider.GetItems(@"C:\Users\paul\OneDrive\scripts");
+
+            DataContext = items;
+        }
+
+        private void TreeView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var i = (Item)tv.SelectedItem;
+            string text;
+
+            text = File.ReadAllText(i.Path);
+            MemoryStream stream = new MemoryStream(ASCIIEncoding.Default.GetBytes(text));
+            TextRange range = new TextRange(rtbText.Document.ContentStart, rtbText.Document.ContentEnd);
+            range.Load(stream, DataFormats.Rtf);
+
+        }
+    }
+}
