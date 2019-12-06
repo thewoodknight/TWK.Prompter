37ae864df38d960c174a47c848f4e60428619f72
Paul Jenkins <paul@thewoodknight.com>
2019-12-06 20:28:38 +1100

    Fixed mirroring in a stupid way

---
+++ TWK.Prompter/Converters/MirrorConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TWK.Prompter.Converters
+{
+    //This is really stupid but works
+    public class MirrorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var _mirror = (int)value;
+            if (_mirror == 0)
+                return 1;
+            else
+                return -1;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
